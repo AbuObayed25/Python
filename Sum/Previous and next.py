ca81759720f8da718b5807783220a6325e7fa09c
Abu Obayed <115519807+AbuObayed25@users.noreply.github.com>
2024-05-13 22:14:33 +0600

    Previous and next.py

---
+++ Sum/Previous and next.py
@@ -0,0 +1,5 @@
+n = int(input())
+prev = n - 1
+next = n + 1
+print("The next number for the number " + str(n) + " is " + str(next) + ".")
+print("The previous number for the number " + str(n) + " is " + str(prev) + ".")
